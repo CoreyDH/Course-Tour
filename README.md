16dc5e4ed365e6135bf4a5206c17fc92ec5a84e3
CoreyDH <rewer6@gmail.com>
2016-08-10 14:26:01 -0500

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Course-Tour
+Intended for Distinct Software Solutions
+
+### Planned Features ###
+- Carousel like interface between holes, and images.
+- Optional multi images/video for each hole.
+- FCK based hole description
+- Single page viewing
+- Easy implementation
+
+### Future Features ###
+- Layout selection
+
+### Dependencies ###
+- jQuery
+- Bootstrap
+- Flexslider 2
--- js/coursetour.js
+++ js/coursetour.js
@@ -1,8 +1,21 @@
 (function($) {
 
+  $('#coursetour-carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    asNavFor: '#coursetour-slider'
+  });
+
   $('#coursetour-slider').flexslider({
     animation: "slide",
-    controlNav: "thumbnails",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: "#coursetour-carousel"
   });
 
 })(jQuery);
