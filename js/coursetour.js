6f6e68204c3a890e436d20df6373cb790fe181e1
CoreyDH <rewer6@gmail.com>
2016-08-11 11:15:08 -0500

    setup framework for coursetour

--- js/coursetour.js
+++ js/coursetour.js
@@ -1,11 +1,21 @@
 (function($) {
 
-  $(".owl-carousel").owlCarousel({
+  $("coursetour .owl-carousel").owlCarousel({
     items:1,
     margin:30,
     stagePadding:30,
     smartSpeed:450
-});
+  });
+
+  $('media .nav-tabs a').each(function() {
+    var $this = $(this);
+    $this.click(function (e) {
+        e.preventDefault();
+        $this.tab('show');
+        $($this.attr('href')).find('.flexslider').resize();
+    });
+  });
+
 
   $('#coursetour-images-carousel').flexslider({
     animation: "slide",
@@ -21,7 +31,6 @@
     animation: "slide",
     controlNav: false,
     animationLoop: false,
-    slideshow: false,
     sync: "#coursetour-images-carousel"
   });
 
@@ -35,12 +44,52 @@
     asNavFor: '#coursetour-videos-slider'
   });
 
-  $('#coursetour-videos-slider').flexslider({
-    animation: 'slide',
-    useCSS: false,
-    controlNav: false,
-    animationLoop: false,
-    sync: "#coursetour-videos-carousel"
-  });
+  $('#coursetour-videos-slider')
+    .flexslider({
+      animation: 'slide',
+      useCSS: false,
+      controlNav: false,
+      animationLoop: false,
+      slideshow: false,
+      sync: "#coursetour-videos-carousel"
+    });
+
+
+    $('#coursetour-images-carousel-2').flexslider({
+      animation: "slide",
+      controlNav: false,
+      animationLoop: false,
+      slideshow: false,
+      itemWidth: 210,
+      itemMargin: 5,
+      asNavFor: '#coursetour-images-slider-2'
+    });
+
+    $('#coursetour-images-slider-2').flexslider({
+      animation: "slide",
+      controlNav: false,
+      animationLoop: false,
+      sync: "#coursetour-images-carousel-2"
+    });
+
+    $('#coursetour-videos-carousel-2').flexslider({
+      animation: "slide",
+      controlNav: false,
+      animationLoop: false,
+      slideshow: false,
+      itemWidth: 210,
+      itemMargin: 5,
+      asNavFor: '#coursetour-videos-slider-2'
+    });
+
+    $('#coursetour-videos-slider-2')
+      .flexslider({
+        animation: 'slide',
+        useCSS: false,
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        sync: "#coursetour-videos-carousel-2"
+      });
 
 })(jQuery);
